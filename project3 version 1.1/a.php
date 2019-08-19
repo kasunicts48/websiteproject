84215c226578ce61058d44a2478ee16e9bd7764d
Kasun Dananjaya <kasundananjayaweerasooriya@gmail.com>
2019-08-19 23:36:04 +0530

    Add files via upload

---
+++ project3 version 1.1/a.php
@@ -0,0 +1,22 @@
+	<div>
+			<input type="text" name="itemName" class="addItem-inputs form-control" placeholder="Item Name"> 
+			 <input type="text" name="ItemPrice" class="addItem-inputs form-control" placeholder="Price"> 
+			 <input type="text" name="desc" class="addItem-inputs form-control" placeholder="Description"> 
+			 <button class="btn btn-success add-btn">Add</button>
+	</div>
+	<div class="nextItem nextItem2">
+
+	</div>
+
+
+
+
+
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+
+
+	<script type="text/javascript">
+		
+	
+	</script>
